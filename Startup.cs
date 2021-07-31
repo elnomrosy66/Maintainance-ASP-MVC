274e25323baa0425db6da2c1a73f35b60e5fb469
elnomrosy66 <elnomrosy66@gmail.com>
2021-07-31 13:08:23 +0200

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Maintainance.Startup))]
+namespace Maintainance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
