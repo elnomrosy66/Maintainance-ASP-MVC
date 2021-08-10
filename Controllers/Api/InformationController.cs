1cf03962af19b1d35420dc2e3eebdf5c2c0b354a
MohamedMmdouh <mohamedmmdouh2022@gmail.com>
2021-08-10 11:38:08 +0200

    add apo

---
+++ Controllers/Api/InformationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Maintainance.Controllers.Api
+{
+    public class InformationController : ApiController
+    {
+        // GET: api/Information
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Information/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Information
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Information/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Information/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
