71175934d81928af175b13b65799296b1f737e64
MohamedMmdouh <mohamedmmdouh2022@gmail.com>
2021-08-14 13:06:22 +0200

    Add Approleservises

---
+++ Controllers/WorkController.cs
@@ -0,0 +1,45 @@
+﻿using Maintainance.Helpers.Image;
+using Maintainance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maintainance.Controllers
+{
+    public class WorkController : Controller
+    {
+        private ApplicationDbContext _context;
+        Image image = new Image();
+        public WorkController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Work
+        public ActionResult Index()
+        {
+            var alldata = _context.ourWorks.ToList();
+            return View(alldata);
+        }
+        
+        public ActionResult CreateWork()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Create(OurWork ourwork)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("index", "work", ourwork);
+            }
+            if (image.insertimage(ourwork))
+            {
+                _context.ourWorks.Add(ourwork);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
